b9456739fdd0ac2dd52f0fb38f76821e8632f66c
Jordan Alexander <jordana@rainn.org>
2014-02-13 03:23:46 -0500

    comment dat file

--- js/map.js
+++ js/map.js
@@ -7,19 +7,29 @@ function Map() {
 
 Map.prototype = {
 	move:function() {
+		//get data attribute for xy location
 		var new_x = this.getAttribute("data-x") - 1;
 		var new_y = this.getAttribute("data-y") - 1;
+		//render new map
 		Map.prototype.render(new_x, new_y);
+		//actions? to-do :fix me
 		Map.prototype.randomAction();
 	},
 	render:function(p_x,p_y) {
+		//grab tile location to track previous location
 		this.prev_y = this.y;
 		this.prev_x = this.x;
 		//check for map boundaries
+		//set new map coordiantes
 		this.x = p_x+1;
 		this.y = p_y+1;
-		document.getElementById("location").textContent = "("+this.x+","+this.y+")";
+		//display new coordinates
+		//fix me
+		//element handling should be separate from the map engine
+		document.getElementById("location").textContent = "("+this.x+","+this.y+")"; 
 		var max_divs = 2;
+
+		//borders
 		if(!p_x) p_x = 1;
 	    if(!p_y) p_y = 1;
 	    if(p_x >= 100) p_x--;
@@ -39,6 +49,7 @@ Map.prototype = {
 	                cell.textContent = "";
 	            }
 	            else {
+	            	//set data attribute
 	                cell.setAttribute("data-x",x);
 	                cell.setAttribute("data-y",y);
 	                //add event handler for the click event (move)
@@ -51,6 +62,7 @@ Map.prototype = {
 	        //add row to table
 	        root.appendChild(row);
 	    }
+	    //grab table
 	    var main = document.getElementById("container");
 	    var old_table = document.getElementById('root_table')
 	    if(old_table) main.removeChild(old_table); 
@@ -58,6 +70,7 @@ Map.prototype = {
 	    main.appendChild(root);
 	},
 	randomAction:function(x,y) {
+		//map actions should be in their own proto object "Actions"
 		var derp = document.querySelector("td[data-x='"+this.x+"'][data-y='"+this.y+"']");
 		derp.style.backgroundColor = "red";
 		if (this.x ==  this.prev_x && this.y == this.prev_y) return;
@@ -67,5 +80,6 @@ Map.prototype = {
 	}
 }
 
+//render dat map
 var map = new Map();
 map.render(0,0);
